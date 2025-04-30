3e556661b0bb364aae5c05527f87344c03dbc910
Xintong Li <sdjnyclxt@163.com>
2025-04-30 16:18:48 +0800

    Update FoodItem.java
    增加id属性

--- app/src/main/java/hk/hku/group_project/database/FoodItem.java
+++ app/src/main/java/hk/hku/group_project/database/FoodItem.java
@@ -8,10 +8,23 @@ public class FoodItem {
     public String expiry;        // 保质期（例如 2025-12-01）
     public String storage;       // 冷藏 / 冷冻
     public double price;
+    public String id;       //存储Firebase自动生成的key
 
     public FoodItem() {} // Firebase 需要空构造函数
 
+    //前端add food时
     public FoodItem(String name, List<String> owners, String expiry, String storage, double price) {
+        this.id = null;
+        this.name = name;
+        this.owners = owners;
+        this.expiry = expiry;
+        this.storage = storage;
+        this.price = price;
+    }
+
+    // 全参数构造
+    public FoodItem(String id,String name, List<String> owners, String expiry, String storage, double price) {
+        this.id = id;
         this.name = name;
         this.owners = owners;
         this.expiry = expiry;
